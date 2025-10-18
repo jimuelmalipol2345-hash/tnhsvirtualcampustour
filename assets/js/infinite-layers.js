02caac8fedefda1bddf5704a155757309da33fa1
jimuelmalipol2345-hash <jimuelmalipol2345@gmail.com>
2025-10-18 18:32:06 +0800

    remove text

--- assets/js/infinite-layers.js
+++ assets/js/infinite-layers.js
@@ -6,15 +6,15 @@ export default class Index {
     this.resize();
     
     this.sources = [
-      {src: 'assets/images/featured/1.jpg', caption: '30 knots <br>12 x 16 inch C type hand print <br>Edition of 1 Plus an additional artist Proof <br>2021'},
-      {src: 'assets/images/featured/2.jpg', caption: 'Sad Mis-Step <br>12 x 16 inch C type hand print <br>Edition of 1 Plus an additional artist Proof <br>2024'},
-      {src: 'assets/images/featured/3.jpg', caption: 'Mini Orange <br>12 x 16 inch C type hand print <br>Edition of 1 Plus an additional artist Proof <br>2014'},
-      {src: 'assets/images/featured/4.jpg', caption: 'After Storm <br>12 x 16 inch C type hand print <br>Edition of 1 Plus an additional artist Proof <br>2022'},
-      {src: 'assets/images/featured/5.jpg', caption: 'Untitled <br>12 x 16 inch C type hand print <br>Edition of 1 Plus an additional artist Proof <br>2016'},
-      {src: 'assets/images/featured/6.jpg', caption: 'Toilet Paper <br>12 x 16 inch C type hand print <br>Edition of 1 Plus an additional artist Proof <br>2022'},
-      {src: 'assets/images/featured/7.jpg', caption: 'Cocoa Eggplant Tomato <br>12 x 16 inch C type hand print <br>Edition of 1 Plus an additional artist Proof <br>2025'},
-      {src: 'assets/images/featured/8.jpg', caption: 'Toilet Paper <br>12 x 16 inch C type hand print <br>Edition of 1 Plus an additional artist Proof <br>2022'},
-      {src: 'assets/images/featured/9.jpg', caption: 'Production Fun Fact (Eggs) <br>12 x 16 inch C type hand print <br>Edition of 1 Plus an additional artist Proof <br>2024'},
+      {src: 'assets/images/featured/1.jpg', caption: ''},
+      {src: 'assets/images/featured/2.jpg', caption: ''},
+      {src: 'assets/images/featured/3.jpg', caption: ''},
+      {src: 'assets/images/featured/4.jpg', caption: ''},
+      {src: 'assets/images/featured/5.jpg', caption: ''},
+      {src: 'assets/images/featured/6.jpg', caption: ''},
+      {src: 'assets/images/featured/7.jpg', caption: ''},
+      {src: 'assets/images/featured/8.jpg', caption: ''},
+      {src: 'assets/images/featured/9.jpg', caption: ''},
     ];
     this.data = [
       {x: 71, y: 58, w: 400, h: 270},
